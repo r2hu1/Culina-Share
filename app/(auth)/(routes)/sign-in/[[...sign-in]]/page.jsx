f26577e12130fb05959c8db7f5eb909bc957e223
r2hu1 <newasram86@gmail.com>
2024-02-21 21:28:23 +0530

    []

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -1,15 +1,5 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return (
-        <div className="absolute top-0 left-0 right-0 px-6 -z-10 h-full w-full flex items-center justify-center">
-            <ClerkLoaded>
-                <SignIn />
-            </ClerkLoaded>
-            <ClerkLoading>
-                <Skeleton className="h-[500px] w-screen max-w-sm" />
-            </ClerkLoading>
-        </div>
-    );
+    return <SignIn />;
 }
