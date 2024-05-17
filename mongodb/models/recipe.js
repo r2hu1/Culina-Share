5cb398bb76a58b4c9d05a9134b1494c72111497a
Rahul Rajput <newasram86@gmail.com>
2024-05-17 12:25:21 +0530

    fix

--- mongodb/models/recipe.js
+++ mongodb/models/recipe.js
@@ -1,6 +1,6 @@
 import { models, model } from "mongoose";
 import { recipeSchema } from "../schema/recipeSchema";
 
-const Recipe = models.Saved || model("Recipe", recipeSchema);
+const Recipe = models.Recipe || model("Recipe", recipeSchema);
 
 export default Recipe; 
